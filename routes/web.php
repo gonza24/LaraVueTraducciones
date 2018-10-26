94bddac481c999425fad5e033a755ae11bacf040
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-10-26 01:11:34 -0300

    Add route and setLanguage method in Controller.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/set_language/{lang}', 'Controller@setLanguage')->name('set_language');
